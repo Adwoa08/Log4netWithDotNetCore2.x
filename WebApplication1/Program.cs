46d112a82b99d6a13a7f56aa111aab82a8624874
Terry Ntow <tntow@ignet.com>
2019-01-25 13:46:29 -0700

    log4netConfiguration

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml;
+using log4net;
+using log4net.Config;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            var logRepo = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepo, new FileInfo("log4net.config"));
+            
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((webHostBuilderContext, loggingBuilder) =>
+                {
+                    // webHostBuilderContext.HostingEnvironment.con
+                    loggingBuilder.AddLog4Net();
+                })
+                .UseStartup<Startup>();
+    }
+}
